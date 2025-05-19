f85593c6dfbe0a82606134a647e065d8729e1dd5
Sunita Nandy <110041267+Sunita1008@users.noreply.github.com>
2025-05-20 03:15:05 +0530

    Add files via upload

---
+++ MVCDesignPattern2/src/main/java/in/sp/dbcon/DBconnection.java
@@ -0,0 +1,22 @@
+package in.sp.dbcon;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBconnection {
+
+	public static Connection getConnection()
+	{
+		Connection con=null;
+		try {
+			
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			 con=DriverManager.getConnection("jdbc:mysql://localhost:3306/mvc_db","root","root");
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		return con;
+	}
+}
